38eebacc80c3344075a4ade1d9cb2a975c420598
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2022-02-15 11:24:29 -0800

    added a feature to sort by price,by price percecentage in 1hr,24hr and 7d

--- src/components/coinList/colinList.js
+++ src/components/coinList/colinList.js
@@ -6,25 +6,40 @@ import {
 	getCoins,
 	sortByPriceAsc,
 	sortByPriceDesc,
+	sortBy1hrPriceAsc,
+	sortBy1hrPriceDesc,
+	sortBy24hrPriceAsc,
+	sortBy24hrPriceDesc,
+	sortBy7DPriceAsc,
+	sortBy7DPriceDesc,
 } from 'redux/coinListReducer/actions';
 import {
 	coinsSelector,
 	errorSelector,
 	loadingSelector,
 	sortPriceAscSelector,
+	sortPriceDescSelector,
+	sortPriceIn1hrAscSelector,
+	sortPriceIn1hrDescSelector,
+	sortPriceIn24hrAscSelector,
+	sortPriceIn24hrDescSelector,
+	sortPriceIn7dAscSelector,
+	sortPriceIn7dDescSelector,
 } from 'redux/coinListReducer';
 
 function kFormatter(num) {
-	if (Math.abs(num) > 999 && Math.abs(num) < 999999) {
+	if (Math.abs(num) < 999) {
+		return Math.sign(num) * Math.abs(num);
+	} else if (Math.abs(num) > 999 && Math.abs(num) < 999999) {
 		return Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'k';
 	} else if (Math.abs(num) > 999999 && Math.abs(num) < 999999999) {
 		return Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + 'M';
-	} else if (Math.abs(num) > 999999999 && Math.abs(num) < 999999999999) {
-		return Math.sign(num) * (Math.abs(num) / 1000000000).toFixed(1) + 'B';
 	} else {
-		return Math.sign(num) * Math.abs(num);
+		return Math.sign(num) * (Math.abs(num) / 1000000000).toFixed(1) + 'B';
 	}
 }
+
+const statusColor = (data) => (data > 0 ? '#00fc2a' : '#fe1040');
 
 const ColinList = () => {
 	const dispatch = useDispatch();
@@ -32,6 +47,29 @@ const ColinList = () => {
 	const isLoading = useSelector(loadingSelector);
 	const error = useSelector(errorSelector);
 	const sortPriceAsc = useSelector(sortPriceAscSelector);
+	const sortPriceDesc = useSelector(sortPriceDescSelector);
+	const sortPriceIn1hrAsc = useSelector(sortPriceIn1hrAscSelector);
+	const sortPriceIn1hrDesc = useSelector(sortPriceIn1hrDescSelector);
+	const sortPriceIn24hrAsc = useSelector(sortPriceIn24hrAscSelector);
+	const sortPriceIn24hrDesc = useSelector(sortPriceIn24hrDescSelector);
+	const sortPriceIn7dAsc = useSelector(sortPriceIn7dAscSelector);
+	const sortPriceIn7dDesc = useSelector(sortPriceIn7dDescSelector);
+
+	const icon = (state1, state2, action1, action2) => (
+		<i
+			className={`fas  ${
+				state1 || (state1 === false && state2 === false)
+					? 'fas fa-sort-down price-asc'
+					: 'fas fa-sort-up price-desc'
+			}`}
+			onClick={() =>
+				state1 || (state1 === false && state2 === false)
+					? dispatch(action1())
+					: dispatch(action2())
+			}
+		/>
+	);
+
 	useEffect(() => {
 		dispatch(getCoins());
 		// eslint-disable-next-line
@@ -40,30 +78,47 @@ const ColinList = () => {
 		<>
 			{error && <h1>Error...</h1>}
 			{isLoading && <h1>Loading...</h1>}
-			<h1>Crypto</h1>
 			<table>
 				<thead>
 					<tr>
 						<th>#</th>
 						<th>Name</th>
-						<th>
-							<i
-								className={`fas  ${
-									sortPriceAsc
-										? 'fas fa-sort-down price-asc'
-										: 'fas fa-sort-up price-desc'
-								}`}
-								onClick={() =>
-									sortPriceAsc
-										? dispatch(sortByPriceAsc())
-										: dispatch(sortByPriceDesc())
-								}
-							/>
-							Price
+						<th className='price'>
+							{icon(
+								sortPriceAsc,
+								sortPriceDesc,
+								sortByPriceAsc,
+								sortByPriceDesc
+							)}
+							&#9; Price
 						</th>
-						<th>1h%</th>
-						<th>24h%</th>
-						<th>7d%</th>
+						<th className='in1hr'>
+							{icon(
+								sortPriceIn1hrAsc,
+								sortPriceIn1hrDesc,
+								sortBy1hrPriceAsc,
+								sortBy1hrPriceDesc
+							)}
+							1h%
+						</th>
+						<th>
+							{icon(
+								sortPriceIn24hrAsc,
+								sortPriceIn24hrDesc,
+								sortBy24hrPriceAsc,
+								sortBy24hrPriceDesc
+							)}
+							24h%
+						</th>
+						<th>
+							{icon(
+								sortPriceIn7dAsc,
+								sortPriceIn7dDesc,
+								sortBy7DPriceAsc,
+								sortBy7DPriceDesc
+							)}
+							7d%
+						</th>
 						<th>24h Volume/Market Cap</th>
 						<th>Circulating/Total Supply</th>
 						<th>Last 7d</th>
@@ -82,13 +137,28 @@ const ColinList = () => {
 									</div>
 								</td>
 								<td>${coin.current_price}</td>
-								<td>
+								<td
+									style={{
+										color: statusColor(
+											coin.price_change_percentage_1h_in_currency
+										),
+									}}>
 									{coin.price_change_percentage_1h_in_currency.toFixed(2)}%
 								</td>
-								<td>
+								<td
+									style={{
+										color: statusColor(
+											coin.price_change_percentage_24h_in_currency
+										),
+									}}>
 									{coin.price_change_percentage_24h_in_currency.toFixed(2)}%
 								</td>
-								<td>
+								<td
+									style={{
+										color: statusColor(
+											coin.price_change_percentage_7d_in_currency
+										),
+									}}>
 									{coin.price_change_percentage_7d_in_currency.toFixed(2)}%
 								</td>
 								<td>
--- src/redux/coinListReducer/index.js
+++ src/redux/coinListReducer/index.js
@@ -3,13 +3,31 @@ export const FETCHING_COINS_SUCCESS = 'FETCHING_COINS_SUCCESS';
 export const FETCHING_COINS_FAIL = 'FETCHING_COINS_FAIL';
 export const SORT_BY_PRICE_ASC = 'SORT_BY_PRICE_ASC';
 export const SORT_BY_PRICE_DESC = 'SORT_BY_PRICE_DESC';
+export const SORT_BY_1HR_PRICE_PERCENTAGE_ASC =
+	'SORT_BY_1HR_PRICE_PERCENTAGE_ASC';
+export const SORT_BY_1HR_PRICE_PERCENTAGE_DESC =
+	'SORT_BY_1HR_PRICE_PERCENTAGE_DESC';
+export const SORT_BY_24HR_PRICE_PERCENTAGE_ASC =
+	'SORT_BY_24HR_PRICE_PERCENTAGE_ASC';
+export const SORT_BY_24HR_PRICE_PERCENTAGE_DESC =
+	'SORT_BY_24HR_PRICE_PERCENTAGE_DESC';
+export const SORT_BY_7D_PRICE_PERCENTAGE_ASC =
+	'SORT_BY_7D_PRICE_PERCENTAGE_ASC';
+export const SORT_BY_7D_PRICE_PERCENTAGE_DESC =
+	'SORT_BY_7D_PRICE_PERCENTAGE_DESC';
 
 const initialState = {
 	coins: [],
 	isLoading: false,
 	error: false,
-	sortPriceAsc: true,
+	sortPriceAsc: false,
 	sortPriceDesc: false,
+	sortPriceIn1hrAsc: false,
+	sortPriceIn1hrDesc: false,
+	sortPriceIn24hrAsc: false,
+	sortPriceIn24hrDesc: false,
+	sortPriceIn7dAsc: false,
+	sortPriceIn7dDesc: false,
 };
 
 const coinListReducer = (state = initialState, action) => {
@@ -53,6 +71,76 @@ const coinListReducer = (state = initialState, action) => {
 				sortPriceAsc: true,
 				sortPriceDesc: false,
 			};
+		case SORT_BY_1HR_PRICE_PERCENTAGE_ASC:
+			const sortCoinBy1hrPriceAsc = state.coins.sort(
+				(a, b) =>
+					a.price_change_percentage_1h_in_currency -
+					b.price_change_percentage_1h_in_currency
+			);
+			return {
+				...state,
+				coins: [...sortCoinBy1hrPriceAsc],
+				sortPriceIn1hrAsc: false,
+				sortPriceIn1hrDesc: true,
+			};
+		case SORT_BY_1HR_PRICE_PERCENTAGE_DESC:
+			const sortCoinBy1hrPriceDesc = state.coins.sort(
+				(a, b) =>
+					b.price_change_percentage_1h_in_currency -
+					a.price_change_percentage_1h_in_currency
+			);
+			return {
+				...state,
+				coins: [...sortCoinBy1hrPriceDesc],
+				sortPriceIn1hrAsc: true,
+				sortPriceIn1hrDesc: false,
+			};
+		case SORT_BY_24HR_PRICE_PERCENTAGE_ASC:
+			const sortCoinBy24hrPriceAsc = state.coins.sort(
+				(a, b) =>
+					a.price_change_percentage_24h_in_currency -
+					b.price_change_percentage_24h_in_currency
+			);
+			return {
+				...state,
+				coins: [...sortCoinBy24hrPriceAsc],
+				sortPriceIn24hrAsc: false,
+				sortPriceIn24hrDesc: true,
+			};
+		case SORT_BY_24HR_PRICE_PERCENTAGE_DESC:
+			const sortCoinBy24hrPriceDesc = state.coins.sort(
+				(a, b) =>
+					b.price_change_percentage_24h_in_currency -
+					a.price_change_percentage_24h_in_currency
+			);
+			return {
+				...state,
+				coins: [...sortCoinBy24hrPriceDesc],
+				sortPriceIn24hrAsc: true,
+				sortPriceIn24hrDesc: false,
+			};
+		case SORT_BY_7D_PRICE_PERCENTAGE_ASC:
+			const sortCoinBy7dPriceAsc = state.coins.sort(
+				(a, b) =>
+					a.price_change_percentage_7d_in_currency -
+					b.price_change_percentage_7d_in_currency
+			);
+			return {
+				coins: [...sortCoinBy7dPriceAsc],
+				sortPriceIn7dAsc: false,
+				sortPriceIn7dDesc: true,
+			};
+		case SORT_BY_7D_PRICE_PERCENTAGE_DESC:
+			const sortCoinBy7dPriceDesc = state.coins.sort(
+				(a, b) =>
+					b.price_change_percentage_7d_in_currency -
+					a.price_change_percentage_7d_in_currency
+			);
+			return {
+				coins: [...sortCoinBy7dPriceDesc],
+				sortPriceIn7dAsc: true,
+				sortPriceIn7dDesc: false,
+			};
 		default:
 			return state;
 	}
@@ -65,3 +153,15 @@ export const loadingSelector = (state) => state.coinList.isLoading;
 export const errorSelector = (state) => state.coinList.error;
 export const sortPriceAscSelector = (state) => state.coinList.sortPriceAsc;
 export const sortPriceDescSelector = (state) => state.coinList.sortPriceDesc;
+export const sortPriceIn1hrAscSelector = (state) =>
+	state.coinList.sortPriceIn1hrAsc;
+export const sortPriceIn1hrDescSelector = (state) =>
+	state.coinList.sortPriceIn1hrDesc;
+export const sortPriceIn24hrAscSelector = (state) =>
+	state.coinList.sortPriceIn24hrAsc;
+export const sortPriceIn24hrDescSelector = (state) =>
+	state.coinList.sortPriceIn24hrDesc;
+export const sortPriceIn7dAscSelector = (state) =>
+	state.coinList.sortPriceIn7dAsc;
+export const sortPriceIn7dDescSelector = (state) =>
+	state.coinList.sortPriceIn7dDesc;
